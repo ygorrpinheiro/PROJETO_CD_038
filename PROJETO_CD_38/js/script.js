768096f30e61e0089715e6def24d3df01ddbf5ee
Ygor Pinheiro <ygorrpinheiro2@gmail.com>
2023-10-01 18:55:51 -0300

    Novas Implementações

--- PROJETO_CD_38/js/script.js
+++ PROJETO_CD_38/js/script.js
@@ -16,7 +16,11 @@ function adicionarEquipamento() {
     cell.textContent = equipamento[keys[i]];
   }
 
-  const actionCell = newRow.insertCell(keys.length);
+  const qrCodeCell = newRow.insertCell(keys.length);
+  const qrCode = generateQRCodeUrl(JSON.stringify(equipamento));
+  qrCodeCell.innerHTML = `<img src="${qrCode}" alt="QR Code">`;
+
+  const actionCell = newRow.insertCell(keys.length + 1);
   const removeButton = document.createElement('button');
   removeButton.classList.add('remove-button');
   removeButton.textContent = 'Remover';
@@ -24,9 +28,48 @@ function adicionarEquipamento() {
     removerEquipamento(newRow);
   });
   actionCell.appendChild(removeButton);
+
+  const printButtonCell = newRow.insertCell(keys.length + 2);
+  const printButton = document.createElement('button');
+  printButton.classList.add('print-button');
+  printButton.textContent = 'Imprimir Etiqueta';
+  printButton.addEventListener('click', function() {
+    imprimirEtiqueta(equipamento);
+  });
+  printButtonCell.appendChild(printButton);
+}
+
+function generateQRCodeUrl(text) {
+  return 'https://chart.googleapis.com/chart?chs=150x150&cht=qr&chl=' + encodeURIComponent(text);
 }
 
 function removerEquipamento(row) {
   const equipamentosTable = document.getElementById('equipamentosTable').getElementsByTagName('tbody')[0];
   equipamentosTable.removeChild(row);
+}
+
+function imprimirEtiqueta(equipamento) {
+  const etiquetaContent = `
+    <h2>Informações do Equipamento</h2>
+    <p><strong>Nome: </strong>${equipamento.nome}</p>
+    <p><strong>Modelo: </strong>${equipamento.modelo}</p>
+    <p><strong>Fabricante: </strong>${equipamento.fabricante}</p>
+
+    <img src="${generateQRCodeUrl(JSON.stringify(equipamento))}" alt="QR Code" />
+  `;
+
+  const etiquetaWindow = window.open('', '_blank');
+  etiquetaWindow.document.write(`
+    <html>
+      <head>
+        <title>Etiqueta do Equipamento</title>
+      </head>
+      <body>
+        ${etiquetaContent}
+      </body>
+    </html>
+  `);
+
+  etiquetaWindow.document.close();
+  etiquetaWindow.print();
 }
