d17a45acce0a64834781572dbb87198a87058c75
Ygor Pinheiro <ygorrpinheiro2@gmail.com>
2023-10-02 20:42:57 -0300

    Att

--- PROJETO_CD_38/js/script.js
+++ PROJETO_CD_38/js/script.js
@@ -49,27 +49,89 @@ function removerEquipamento(row) {
 }
 
 function imprimirEtiqueta(equipamento) {
-  const etiquetaContent = `
-    <h2>Informações do Equipamento</h2>
-    <p><strong>Nome: </strong>${equipamento.nome}</p>
-    <p><strong>Modelo: </strong>${equipamento.modelo}</p>
-    <p><strong>Fabricante: </strong>${equipamento.fabricante}</p>
+  const qrCodeUrl = generateQRCodeUrl(JSON.stringify(equipamento));
 
-    <img src="${generateQRCodeUrl(JSON.stringify(equipamento))}" alt="QR Code" />
+  const zplContent = `
+    ^XA
+    ^POI
+    ^LL400
+    ^PW700
+    ^LS0
+    ^CF0,30^FO30,50^FDInformacoes do Equipamento^FS
+    ^CF0,20^FO30,100^FDAppliance:^FS
+    ^CF0,20^FO180,100^FD${equipamento.equipamento}^FS
+    ^CF0,20^FO30,150^FDMarca:^FS
+    ^CF0,20^FO180,150^FD${equipamento.marca}^FS
+    ^CF0,20^FO30,200^FDModelo:^FS
+    ^CF0,20^FO180,200^FD${equipamento.modelo}^FS
+    ^CF0,20^FO30,250^FDTipo:^FS
+    ^CF0,20^FO180,250^FD${equipamento.tipo}^FS
+    ^CF0,20^FO30,300^FDPatrimônio:^FS
+    ^CF0,20^FO180,300^FD${equipamento.patrimonio}^FS
+    ^CF0,20^FO30,350^FDSérie:^FS
+    ^CF0,20^FO180,350^FD${equipamento.serie}^FS
+    ^CF0,20^FO30,400^FDObservação:^FS
+    ^CF0,20^FO180,400^FD${equipamento.observacao}^FS
+    ^FO300,60^BQ,2,5
+    ^FDQA,${qrCodeUrl}^FS
+    ^XZ
   `;
 
-  const etiquetaWindow = window.open('', '_blank');
-  etiquetaWindow.document.write(`
+  const printWindow = window.open('', '_blank');
+  printWindow.document.write(`
     <html>
       <head>
         <title>Etiqueta do Equipamento</title>
       </head>
       <body>
-        ${etiquetaContent}
+        <pre>${zplContent}</pre>
+        <script>
+          // Print the ZPL content
+          window.onload = function() {
+            window.print();
+          };
+        </script>
       </body>
     </html>
   `);
 
-  etiquetaWindow.document.close();
-  etiquetaWindow.print();
+  printWindow.document.close();
+}
+
+
+
+  function consultaEquipamento() {
+    const patrimonioConsulta = document.getElementById('patrimonioConsulta').value;
+    const serieConsulta = document.getElementById('serieConsulta').value;
+      // Obtém o número de patrimônio para consulta
+
+    // Lógica para buscar o equipamento com o número de patrimônio fornecido
+    // Por exemplo, você pode percorrer a lista de equipamentos e encontrar o correspondente pelo número de patrimônio.
+    // Vou simular uma resposta para esse exemplo.
+    const equipamentoEncontrado = {
+        equipamento: 'Equipamento de Exemplo',
+        marca: 'Marca Exemplo',
+        modelo: 'Modelo Exemplo',
+        tipo: 'Tipo Exemplo',
+        patrimonio: patrimonioConsulta,
+        serie: serieConsulta,
+        observacao: 'Observação Exemplo'
+    };
+    
+
+    // Exibindo o equipamento encontrado na tabela
+    const tbody = document.querySelector('#equipamentosTable tbody');
+    tbody.innerHTML = '';  // Limpa o conteúdo anterior da tabela
+
+    const newRow = tbody.insertRow();
+    newRow.innerHTML = `
+        <td>${equipamentoEncontrado.equipamento}</td>
+        <td>${equipamentoEncontrado.marca}</td>
+        <td>${equipamentoEncontrado.modelo}</td>
+        <td>${equipamentoEncontrado.tipo}</td>
+        <td>${equipamentoEncontrado.patrimonio}</td>
+        <td>${equipamentoEncontrado.serie}</td>
+        <td>${equipamentoEncontrado.observacao}</td>
+    `;
+
 }
